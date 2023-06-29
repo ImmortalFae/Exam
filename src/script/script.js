02b06baf8f3135137102f11a14adbfcd40001e84
ImmortalFae <96302915+ImmortalFae@users.noreply.github.com>
2023-06-29 04:28:22 -0700

    exam

---
+++ src/script/script.js
@@ -0,0 +1,40 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
+
+  ///  shopping cart ///
+
+  const product = [
+{
+  id: 0,
+  image: 'img/05.jpg',
+  title: 'surati',
+  price: 120,
+},
+{
+  id: 2,
+  image: 'img/06.jpg',
+  title: 'surati',
+  price: 120,
+},
+{
+id: 3,
+image: 'img/06.jpg',
+title: 'surati',
+price: 120,
+}
+];
